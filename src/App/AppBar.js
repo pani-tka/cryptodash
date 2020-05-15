cbefc7970af7178d2f384a602d0fe3f9ebdef641
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2020-05-15 11:41:35 +0300

    Added AppProvider

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import { AppContext } from './AppProvider';
 
 const Logo = styled.div`
   font-size: 1.5em;
@@ -16,12 +17,25 @@ const ControlButtonElem = styled.div`
   ${props =>
     props.active &&
     css`
-      text-shadow: 0px 0px 50px blue;
+      text-shadow: 0px 0px 50px aquamarine;
     `}
 `;
-
-function ControlButton({ name, active }) {
-  return <ControlButtonElem active={active}>{name}</ControlButtonElem>;
+function toProperCase(lower) {
+  return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+function ControlButton({ name }) {
+  return (
+    <AppContext.Consumer>
+      {({ page, setPage }) => (
+        <ControlButtonElem
+          active={page === name}
+          onClick={() => setPage(name)}
+        >
+          {toProperCase(name)}
+        </ControlButtonElem>
+      )}
+    </AppContext.Consumer>
+  );
 }
 
 export default function () {
@@ -29,8 +43,8 @@ export default function () {
     <Bar>
       <Logo> CryptoDash </Logo>
       <div />
-      <ControlButton active name="Dashboard" />
-      <ControlButton name="Settings" />
+      <ControlButton active name="dashboard" />
+      <ControlButton name="settings" />
     </Bar>
   );
 }
