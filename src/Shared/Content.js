30fa5a803835ba6f12fb2adfd702a1f9a8178839
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2020-05-19 00:09:38 +0300

    Add fetch prices data

--- src/Shared/Content.js
+++ src/Shared/Content.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { AppContext } from '../App/AppProvider';
 
 export default function (props) {
-  return <AppContext.Consumer>
-      {({ coinList }) => {
+  return (
+    <AppContext.Consumer>
+      {({ coinList, prices, firstVisit }) => {
         if (!coinList) {
           return <div> Loading Coins </div>;
         }
-        return <div> {props.children} </div>
+        if (!firstVisit && !prices) {
+          return <div> Loading prices </div>;
+        }
+        return <div> {props.children} </div>;
       }}
+      }
     </AppContext.Consumer>
+  );
 }
