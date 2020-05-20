95b81e661fbbf41168ac295872e6888beca09a09
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2020-05-20 14:05:29 +0300

    Updates

---
+++ precache-manifest.5b0e9faf48062ab44bee9a3f5afce4ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7491bb48bf01d3e3798f39ce933565cb",
+    "url": "/cryptodash/index.html"
+  },
+  {
+    "revision": "4b3ea2f94955183c713e",
+    "url": "/cryptodash/static/css/main.f3589c88.chunk.css"
+  },
+  {
+    "revision": "c1ffd9cff3002a439c16",
+    "url": "/cryptodash/static/js/2.0b5800bf.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/cryptodash/static/js/2.0b5800bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b3ea2f94955183c713e",
+    "url": "/cryptodash/static/js/main.423eeb90.chunk.js"
+  },
+  {
+    "revision": "a4557b01c8185771facb",
+    "url": "/cryptodash/static/js/runtime-main.83ad6dfc.js"
+  }
+]);
